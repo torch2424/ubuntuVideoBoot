69835b511b16fc06e19bdf2121cf3919a4b5c622
Aaron Turner <aaron@aaronthedev.com>
2016-10-01 03:57:32 -0700

    Used my home directory

--- vaporBoot.sh
+++ vaporBoot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #Randomly Play a video in a direcory
-vlc `find ~/vagrantBox/source/vaporVideoBoot/bootVideos -type f | shuf -n 1` -f --play-and-exit
+vlc `find /home/aaron/vagrantBox/source/vaporVideoBoot/bootVideos -type f | shuf -n 1` -f --play-and-exit
